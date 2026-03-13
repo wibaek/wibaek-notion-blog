929cda90b31e3105dc6b1bb156f8fbdef10c4de0
Wibaek Park <devmuromi@gmail.com>
2026-03-13 13:12:55 +0900

    🌮

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,6 @@ import 'styles/notion.css'
 import 'styles/prism-theme.css'
 
 import type { AppProps } from 'next/app'
-import { GoogleAnalytics } from '@next/third-parties/google'
 import * as Fathom from 'fathom-client'
 import { useRouter } from 'next/router'
 import { posthog } from 'posthog-js'
@@ -62,10 +61,5 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return (
-    <>
-      <Component {...pageProps} />
-      <GoogleAnalytics gaId='G-PKCWZTWVLX' />
-    </>
-  )
+  return <Component {...pageProps} />
 }
